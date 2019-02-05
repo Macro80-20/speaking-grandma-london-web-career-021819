9e49682163019b965909251529527d8061f1a4b3
fernando <fernando_lopezc@live.com>
2019-02-05 10:26:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,15 +10,15 @@ elsif (some=="WHAT DID YOU EAT TODAY?")
   puts "NO, NOT SINCE 1938!"
 elsif (some== "WHAT?")
   puts "NO, NOT SINCE 1938!"
-  
+
 end
-    
+
   end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
